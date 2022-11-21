21bad67bb4b1830422cf4089ef99b93c65c03bc4
Joao-v-almeida <jaovittor.almeida@gmail.com>
2022-11-21 20:35:27 -0300

    teste

--- src/main/java/br/com/oticanewlook/oticanewlook/OticanewlookApplication.java
+++ src/main/java/br/com/oticanewlook/oticanewlook/OticanewlookApplication.java
@@ -2,6 +2,7 @@ package br.com.oticanewlook.oticanewlook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class OticanewlookApplication {
@@ -9,5 +10,10 @@ public class OticanewlookApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OticanewlookApplication.class, args);
 	}
+
+	@GetMapping("/")
+    public String login() {
+        return "home/login";
+    }
 
 }
