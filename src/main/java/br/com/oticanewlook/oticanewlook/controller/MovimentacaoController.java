448dac3396302532815e8ee55e6ef95da422b401
Joao-v-almeida <jaovittor.almeida@gmail.com>
2022-11-28 21:26:57 -0300

    Restrição de acesso

--- src/main/java/br/com/oticanewlook/oticanewlook/controller/MovimentacaoController.java
+++ src/main/java/br/com/oticanewlook/oticanewlook/controller/MovimentacaoController.java
@@ -2,6 +2,7 @@ package br.com.oticanewlook.oticanewlook.controller;
 
 import java.io.UnsupportedEncodingException;
 import java.util.List;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -10,8 +11,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.oticanewlook.oticanewlook.model.FuncionarioModel;
 import br.com.oticanewlook.oticanewlook.model.MovimentacaoModel;
 import br.com.oticanewlook.oticanewlook.services.CookieService;
+import br.com.oticanewlook.oticanewlook.services.FuncionarioService;
 import br.com.oticanewlook.oticanewlook.services.MovimentacaoService;
 
 @Controller
@@ -20,14 +24,28 @@ public class MovimentacaoController {
     
     @Autowired
     private MovimentacaoService movimentacaoService;
+    
+    @Autowired
+    private FuncionarioService funcionarioService;
 
     @GetMapping("/movimentacoes")
      public String cliente(Model model, HttpServletRequest request) throws UnsupportedEncodingException {
 
-        model.addAttribute("nome", CookieService.getCookie(request, "funcionarioNome"));
+         model.addAttribute("nome", CookieService.getCookie(request, "funcionarioNome"));
 
-         List<MovimentacaoModel> movimentacoes = movimentacaoService.findlAllEntrada();
-         model.addAttribute("movimentacoes", movimentacoes);
-         return "generico/movimentacao";
+         var funcionarioID = CookieService.getCookie(request, "funcionarioID");
+         int funcID = Integer.parseInt(funcionarioID);
+
+         Optional<FuncionarioModel> funcionario = funcionarioService.findById(funcID);
+
+         var tipoFuncionario = funcionario.get().getId_tipo_func();
+
+         if (tipoFuncionario == 1) {
+             List<MovimentacaoModel> movimentacoes = movimentacaoService.findlAllEntrada();
+             model.addAttribute("movimentacoes", movimentacoes);
+             return "generico/movimentacao";
+         }
+         
+         return "generico/acessoNegado";
      }
 }
